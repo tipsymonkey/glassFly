232923a8ec97ba6ebb77124d80c34243bcfa7627
Rafael <raf@tipsymonkey.com>
2013-08-29 11:49:54 -0700

    trying out my first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$app_name = "glassFly";
+
+
+
+$base_url = "http://beta.couchjunkie.com/glass/temp/";
+
+$sfly_base_url = "https://ws.shutterfly.com";
+$sfly_app_id = "54ddc0fe67bf02cbe14497eda8dc1b3d";
+$sfly_shared_secret = "163d5ccd850ac24c";
+
+
+?>
