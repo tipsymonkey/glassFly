7320e8a3c0d5ff92f9eebb810b2eab3d8e5d7fad
Rafael <raf@tipsymonkey.com>
2013-09-04 16:32:49 -0700

    function for user auth beginnings

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 require_once 'config.php';
 
 function getSigniture ($input_url)
@@ -21,21 +18,51 @@ function getTimeStamp ()
 		return $date->format('c');
 }
 
-function sflyAuthUser()
+function curl_post($url,$post_xml)
+{
+
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+  curl_setopt($curl, CURLOPT_POST, 1);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $post_xml);
+  curl_setopt($curl, CURLOPT_HEADER, true);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec ($curl);
+  curl_close ($curl);
+
+  
+  return $response;
+
+}
+
+// Returns the sfly auth token
+// Takes user email address and password. 
+
+function sflyAuthUser($user_email, $user_password)
 {
   // Pull the appid and the base url from the config.php
   global $sfly_app_id, $sfly_base_url;
+  $auth_url = "/user/".$user_email."/auth?oflyAppId=".$sfly_app_id;
+  $url = $sfly_base_url.$auth_url;
+ 
+
+  $curl_response = curl_post($url,writeXML($user_password));
+  echo $curl_response;
+
+
 }
 
 
 // writeXML
 // Returns a string with the xml requested to post in body
 
-function writeXML()
+function writeXML($user_password)
 {
-$dom = new DOMDocument('1.0','utf-8');
+$dom = new DOMDocument('1.0','UTF-8');
 
-$element = $dom->createElementNS('http://www.w3.org/2005/Atom','element');
+$element = $dom->createElementNS('http://www.w3.org/2005/Atom','entry');
 $element->setAttribute('xmlns:user','http://user.openfly.shutterfly.com/v1.0');
 $dom->appendChild($element);
 
@@ -44,49 +71,16 @@ $category->setAttribute('term','user');
 $category->setAttribute('scheme','http://openfly.shutterfly.com/v1.0');
 $category->appendChild($dom->createTextNode(''));
 
-$userpass = $element->appendChild($dom->createElement('user:password','password'));
+$userpass = $element->appendChild($dom->createElement('user:password',$user_password));
 
 return $dom->saveXML();
+
+
 }
 
 
-/*
-    This is going to be debug section stuff. This doens't need to exist / only for now. 
+sflyAuthUser('raf@test94.com','test123');
 
 
-$xmlstr = <<< XML
-<?xml version="1.0" encoding="UTF-8"?>
-<entry xmlns="http://www.w3.org/2005/Atom" xmlns:user="http://user.openfly.shutterfly.com/v1.0">
-  <category term="user" scheme="http://openfly.shutterfly.com/v1.0" />
-  <user:password>sNf86VJG6N</user:password>
-</entry>
-XML;
-
-*/
-
-$auth_url = "/user/ravasquez@shutterfly.com/auth?oflyAppId=".$sfly_app_id;
-
-
-
-
-/*
-
-$url = $sfly_base_url.$auth_url;
-
-
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $xmlstr);
-curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec ($curl);
-curl_close ($curl);
-
-echo $response;
-
-*/
-writeXML();
 
 ?>
