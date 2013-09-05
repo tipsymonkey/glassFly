0b478e9d104dea2be8e68759734be35d0fd2e23f
Rafael <raf@tipsymonkey.com>
2013-09-04 18:23:03 -0700

    xml parsing debug

--- index.php
+++ index.php
@@ -2,24 +2,20 @@
 
 require_once 'config.php';
 
-function getSigniture ($input_url)
-{
+function getSigniture ($input_url) {
 	$time_stamp = getTimeStamp();
 	$base_string = $sfly_shared_secret.$input_url."&oflyHashMeth=MD5&oflyTimestamp=".$time_stamp;
 	return md5($base_string);
-
 }
 
 
-function getTimeStamp ()
-{
+function getTimeStamp () {
 		$date =	new DateTime();
         // Using format 'c' will do the right ISO date format needed. 
 		return $date->format('c');
 }
 
-function curl_post($url,$post_xml)
-{
+function curl_post($url,$post_xml) {
 
 
   $curl = curl_init();
@@ -27,21 +23,19 @@ function curl_post($url,$post_xml)
   curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
   curl_setopt($curl, CURLOPT_POST, 1);
   curl_setopt($curl, CURLOPT_POSTFIELDS, $post_xml);
-  curl_setopt($curl, CURLOPT_HEADER, true);
+  curl_setopt($curl, CURLOPT_HEADER, false); //Turn off  headers in the response. 
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec ($curl);
   curl_close ($curl);
 
   
   return $response;
-
 }
 
 // Returns the sfly auth token
 // Takes user email address and password. 
 
-function sflyAuthUser($user_email, $user_password)
-{
+function sflyAuthUser($user_email, $user_password) {
   // Pull the appid and the base url from the config.php
   global $sfly_app_id, $sfly_base_url;
   $auth_url = "/user/".$user_email."/auth?oflyAppId=".$sfly_app_id;
@@ -49,17 +43,14 @@ function sflyAuthUser($user_email, $user_password)
  
 
   $curl_response = curl_post($url,writeXML($user_password));
-  echo $curl_response;
-
-
+  return $curl_response;
 }
 
 
 // writeXML
 // Returns a string with the xml requested to post in body
 
-function writeXML($user_password)
-{
+function writeXML($user_password) {
 $dom = new DOMDocument('1.0','UTF-8');
 
 $element = $dom->createElementNS('http://www.w3.org/2005/Atom','entry');
@@ -74,13 +65,23 @@ $category->appendChild($dom->createTextNode(''));
 $userpass = $element->appendChild($dom->createElement('user:password',$user_password));
 
 return $dom->saveXML();
-
-
 }
 
 
-sflyAuthUser('raf@test94.com','test123');
 
 
+$response = sflyAuthUser('raf@test94.com','test123');
+echo $response;
+$entry = new SimpleXMLElement($response);
+
+
+echo "<p>----------<p>";
+$namespaces = $entry->getNamespaces(true);
+$child_test = $entry->children("user",TRUE);
+
+foreach ($child_test as $child_node) {
+  echo $child_node->getName()."<br>";
+}
+echo $child_test->newAuthToken;
 
 ?>
